35eb226bd1d88324bcaab655b4c485bc9172725f
Utsav <utsavpokemon9000chatterjee@gmail.com>
2022-10-15 12:41:20 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="weather",
+    version="1.0.0",
+    description="This is a Weather Provider.",
+    author="Utsav",
+    packages=['weather'],
+    install_requires=['requests']
+)
